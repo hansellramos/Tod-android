f71554ddb65f7f35fe951a234cb6b73f33aa3209
Hansell Ramos <hansell.x.ramos.-nd@disney.com>
2017-09-04 18:39:41 -0500

    Added Sign in form Checked session in splash activity

--- app/src/main/java/com/hansellramos/tod/android/SplashScreenActivity.java
+++ app/src/main/java/com/hansellramos/tod/android/SplashScreenActivity.java
@@ -4,20 +4,48 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        init();
+
+        validateLogin();
+
+    }
+
+    private void init() {
+        //Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void validateLogin() {
+        if (mFirebaseAuth != null) {
+            if (mFirebaseAuth.getCurrentUser() != null) {
+                goTo(MainActivity.class);
+            } else {
+                goTo(SignInActivity.class);
+            }
+        } else {
+            Toast.makeText(SplashScreenActivity.this, getText(R.string.error_initializing_auth), Toast.LENGTH_LONG);
+        }
+    }
+
+    private void goTo(final Class activityClass) {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashScreenActivity.this, activityClass));
                 finish();
             }
         },1000);
-
     }
 }
