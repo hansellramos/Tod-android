dbde3c0e040d0c2f7dd34d1e0e0ec0228832bf21
Hansell Ramos <hansell.x.ramos.-nd@disney.com>
2017-09-26 16:58:57 -0500

    Added Navigation drawer files

---
+++ app/src/main/java/com/hansellramos/tod/android/HelpActivity.java
@@ -0,0 +1,80 @@
+package com.hansellramos.tod.android;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HelpActivity extends AppCompatActivity {
+
+    private static final String TAG = "HelpActivity";
+    private final int RC_SIGN_IN = 2905;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mFirebaseAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        init();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mFirebaseAuth != null && mFirebaseAuthStateListener != null) {
+            mFirebaseAuth.addAuthStateListener(mFirebaseAuthStateListener);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mFirebaseAuth != null && mFirebaseAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mFirebaseAuthStateListener);
+        }
+    }
+
+    private void init() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mFirebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.i(TAG, "Signed In: " + user.getDisplayName() + " - " + user.getEmail());
+                    Toast.makeText(HelpActivity.this, "Signed In: " + user.getDisplayName() + " - " + user.getEmail(), Toast.LENGTH_LONG).show();
+                } else {
+                    startActivity(new Intent(HelpActivity.this, SignInActivity.class));
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuItemSignOut:
+                mFirebaseAuth.signOut();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/hansellramos/tod/android/MainActivity.java
+++ app/src/main/java/com/hansellramos/tod/android/MainActivity.java
@@ -1,80 +1,101 @@
 package com.hansellramos.tod.android;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-    private final int RC_SIGN_IN = 2905;
-
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mFirebaseAuthStateListener;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        init();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (mFirebaseAuth != null && mFirebaseAuthStateListener != null) {
-            mFirebaseAuth.addAuthStateListener(mFirebaseAuthStateListener);
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mFirebaseAuth != null && mFirebaseAuthStateListener != null) {
-            mFirebaseAuth.removeAuthStateListener(mFirebaseAuthStateListener);
-        }
-    }
-
-    private void init() {
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
-        mFirebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Log.i(TAG, "Signed In: " + user.getDisplayName() + " - " + user.getEmail());
-                    Toast.makeText(MainActivity.this, "Signed In: " + user.getDisplayName() + " - " + user.getEmail(), Toast.LENGTH_LONG).show();
-                } else {
-                    startActivity(new Intent(MainActivity.this, SignInActivity.class));
-                    finish();
-                }
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-        };
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menuItemSignOut:
-                mFirebaseAuth.signOut();
-            default:
-                return super.onOptionsItemSelected(item);
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
